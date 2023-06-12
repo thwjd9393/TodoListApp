4e6118d087720a6bfd41c8aedf1e3e258030a0b3
thwjd9393 <thwjd9393@naver.com>
2023-06-13 00:40:07 +0900

    update) 탭레이아웃 연동 완료

--- app/src/main/java/com/jscompany/tp16todolistapp/view/MainActivity.kt
+++ app/src/main/java/com/jscompany/tp16todolistapp/view/MainActivity.kt
@@ -3,13 +3,15 @@ package com.jscompany.tp16todolistapp.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayoutMediator
 import com.jscompany.tp16todolistapp.R
 import com.jscompany.tp16todolistapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     //탭이름
-    var titles: MutableList<String> = mutableListOf("진행중", "완료")
+    var tabTitle: MutableList<String> = mutableListOf("진행중", "완료")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +28,18 @@ class MainActivity : AppCompatActivity() {
 
         //3. 뷰 페이저에 아답터 연결
         binding.viewPager.adapter = tabAdapter
+
+        //4. 프래그먼트 페이지 붙이기
+        binding.viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+            }
+        })
+
+        //5. 탭 이름 연동
+        TabLayoutMediator(binding.tabLayout,binding.viewPager) { tab, position ->
+            tab.text = tabTitle.get(position).toString()
+        }.attach()
 
     }
 }
