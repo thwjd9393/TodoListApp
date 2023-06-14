a2433e713bb77a45f08f2c0b8bd5ff921077ce82
thwjd9393 <thwjd9393@naver.com>
2023-06-15 01:49:43 +0900

    update) sql 작업중

--- app/src/main/java/com/jscompany/tp16todolistapp/model/DBHelper.kt
+++ app/src/main/java/com/jscompany/tp16todolistapp/model/DBHelper.kt
@@ -1,36 +1,40 @@
 package com.jscompany.tp16todolistapp.model
 
 import android.annotation.SuppressLint
+import android.content.ContentValues
 import android.content.Context
+import android.content.Context.MODE_PRIVATE
 import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteDatabase.openOrCreateDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.util.Log
 
 class DBHelper(
     context: Context,
-) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+) : SQLiteOpenHelper(context, TABLE_NAME, null, DATABASE_VERSION) {
     companion object{
         const val DATABASE_VERSION = 1
-        const val DATABASE_NAME = "TODO"
+        const val TABLE_NAME = "TODO"
 
         // Table
-        const val NO = "NO" //프라이머리키
+        const val NUM = "num" //프라이머리키
         const val STATUS = "status"
         const val TITLE = "title"
         const val CONTENT = "content"
+
     }
 
     override fun onCreate(db: SQLiteDatabase?) {
-        val sql : String = "CREATE TABLE IF NOT EXISTS TODO(" +
-                "num INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                "status INTEGER," +
-                "title TEXT, " +
-                "content TEXT)"
+        val sql : String = "CREATE TABLE IF NOT EXISTS $TABLE_NAME (" +
+                "$NUM INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "$STATUS TEXT, " +
+                "$TITLE TEXT, " +
+                "$CONTENT TEXT)"
         db?.execSQL(sql)
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
-        val sql = "DROP TABLE IF EXISTS TODO"
+        val sql = "DROP TABLE IF EXISTS $TABLE_NAME"
 
         db?.execSQL(sql)
         onCreate(db)
@@ -63,11 +67,17 @@ class DBHelper(
     fun insertTodo(status: Int, title: String, content:String) {
 
         Log.d("TAG", "${status} , ${title}")
-        val db = this.writableDatabase
-        val query = "INSERT INTO TODO(status, title, content) " +
-                "VALUES ('"+status+"', '"+title+"', '"+content+"')"
+        val db = this.writableDatabase //데이터베이스를 오픈 외부에서 받아온 데이터를 contentValues로 묶음
+//        val query = "INSERT INTO $TABLE_NAME ($STATUS, $TITLE, $CONTENT) " +
+//                "VALUES ('"+status+"', '"+title+"', '"+content+"')"
+//        db.execSQL(query)
 
-        db.execSQL(query)
+        val contentValues = ContentValues().apply {
+            put(STATUS, status.toString())
+            put(TITLE, title)
+            put(CONTENT, content)
+        }
+        db.insert(TABLE_NAME, null, contentValues)
         db.close()
     }
 
